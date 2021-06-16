7faabfa19e8e0f46d2cbb61a028d4c8006cf191b
Adheesh1997 <adheeshkulathunga@gmail.com>
2021-06-17 02:06:39 +0530

    Code was tested and some bugs were debugged

--- FYProject/FYProject/lightpathSettingup.cpp
+++ FYProject/FYProject/lightpathSettingup.cpp
@@ -8,86 +8,6 @@
 using namespace std;
 
 
-/************  Begining of the lightpathNodeID class  ************/
-
-/*void lightpathNodeID :: setID(int ID)
-{
-	id = ID;
-}*/
-
-
-
-
-/************  End of the lightpathNodeID class  ************/
-
-
-
-
-/************  Begining of the lightpathSetup class  ************/
-/*
-lightpathSetup::lightpathSetup()
-{
-	numberOfLighpaths = 0;
-}
-
-void lightpathSetup::establishLightpath(vector<int> nodeList, string wavelengthSt, lightpathSetup &Obj1)
-{
-	LightPathDetails newLightPath;
-	
-	
-	newLightPath.LPnodeVector = {nodeList.begin(), nodeList.end()}; //Shortestpath for lightpath given by dijkstra's algorithm is stored
-	int numberOfNodes = newLightPath.LPnodeVector.size();           //Number of nodes in the shortespath for the lightpath
-	lightpathNodeID sourceAndDestNodes[2];                          //Object array of size 2 for source node and destination node
-	
-
-
-	
-	sourceAndDestNodes[0].setID(newLightPath.LPnodeVector[0]);                   //Assigning the id of the source node
-	//sourceAndDestNodes[0].setTheListOfLightpaths(newLightPath.LPnodeVector);     //Assigning the list of nodes in LP for source node
-	setSearchVector(newLightPath.LPnodeVector[0], nodeList, Obj1);
-
-	sourceAndDestNodes[1].setID(newLightPath.LPnodeVector[numberOfNodes - 1]);   //Assigning the id of the destination node
-	//sourceAndDestNodes[1].setTheListOfLightpaths(newLightPath.LPnodeVector);     //Assigning the list of nodes in LP for destination node
-	
-	//sourceAndDestNodes[0].printTheListOfLP();                       //Print the list of lightpaths for the source node
-	//sourceAndDestNodes[1].printTheListOfLP();                       //Print the list of lightpaths for the destination node
-	
-	newLightPath.sourceNode = &sourceAndDestNodes[0];               //Assining the address of source node
-	newLightPath.destinationNode = &sourceAndDestNodes[1];          //Assining the address of destination node
-	
-	
-	
-
-	stringstream geek(wavelengthSt);
-	geek >> newLightPath.wavelength;                                //Convert the string value of wavelength to integer
-
-	lightPaths.push_back(newLightPath);                             //Push back the lightpath to the lightpath vector
-	numberOfLighpaths++;                                            //Increment the lighpath counter
-}
-
-void lightpathSetup :: setSearchVector(int id, vector<int> temp, lightpathSetup &Obj1)
-{
-	bool check = true;
-	for (int i = 0; i < searchVector.size(); i++)
-	{
-		if (searchVector[i].id == id)
-		{
-			Obj1.searchVector[i].v1.push_back(temp);
-			check = false;
-			break;
-		}
-	}
-	if (check)
-	{
-		lightpathsOfANode tempObj2;
-		tempObj2.id = id;
-		tempObj2.v1.push_back(temp);
-		Obj1.searchVector.push_back(tempObj2);
-	}
-	searchVector;
-}*/
-/************  End of the lightpathSetup class  ************/
-
 lightNode::lightNode(int ID)
 {
 	id = ID;
@@ -133,6 +53,7 @@ void lightpathNetwork::viewAllLighpaths()
 	{
 		cout << "Lighpath node id - " << lighpaths[i].returnId() << endl;
 		lighpaths[i].viewLPlinks();
+		cout << endl << endl;
 	}
 }
 
@@ -185,7 +106,7 @@ void lightpathNetwork::setANewLighpath(vector<int> shortestPath, string waveleng
 
 	else
 	{
-		if (check2 = -1)
+		if (check2 == -1)
 		{
 			lightNode lpNodeD(shortestPath[vecSize - 1]);
 
--- FYProject/FYProject/lightpathSettingup.h
+++ FYProject/FYProject/lightpathSettingup.h
@@ -5,49 +5,6 @@
 #include "graph.h"
 using namespace std;
 
-/*class lightpathNodeID
-{
-	private:
-		int id;                                          //id of the node
-		//vector<vector<int>> lightpathsForTheNode;        //List of lightpaths connected the above node
-
-	public:
-		void setID(int ID);                              //Give and ID value for id
-		//void setTheListOfLightpaths(vector<int> v1);     //Establish the lighpath
-		//void printTheListOfLP();                         //Print the list of lightpaths
-};
-
-struct LightPathDetails
-{
-	lightpathNodeID* sourceNode;            //Source node of the lightpath
-	lightpathNodeID* destinationNode;       //Destination node of the lightpath
-	vector<int> LPnodeVector;               //A vector to store the nodes of the lightpath
-	int wavelength;                         //Wavlength of the lightpath
-	int allocatedBandwidh;                  //Total capacity of the lightpath
-	int availableBandwidth;                 //Available capacity of the lightpath
-	int numberOfLSPs;                       //Total number of LSPs in the lightpath
-};
-
-struct lightpathsOfANode
-{
-	int id;
-	vector<vector<int>> v1;
-};
-
-
-class lightpathSetup
-{
-	private:
-		vector<LightPathDetails> lightPaths;    //All the lighpaths are stored in this vector
-		vector<lightpathsOfANode> searchVector;
-		int numberOfLighpaths;                  //Counter to count the number of established lightpaths
-		lightpathsOfANode obj1;
-
-	public:
-		lightpathSetup();                           //Constructor to set the above lighpath counter to zero
-		void establishLightpath(vector<int> nodeList, string wavelengthSt, lightpathSetup &Obj1);     //function which establishes a lightpath
-		void setSearchVector(int id, vector<int> temp, lightpathSetup &Obj1);
-};*/
 
 class lightNode
 {
