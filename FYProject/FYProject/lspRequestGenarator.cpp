52c3ed1b7e16a0c13146c1f20a5e2b2b7b42d9b9
yapa-ymtl <lakmaltharindu009@gmail.com>
2021-06-26 11:47:34 +0530

    LSP requests generator is done

---
+++ FYProject/FYProject/lspRequestGenarator.cpp
@@ -0,0 +1,57 @@
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <iostream>
+
+#include "lspRequestGenarator.h"
+
+
+using namespace std;
+
+
+lspRequestGenarator::lspRequestGenarator(int numberOfNode)
+{
+    numOfNode = numberOfNode;
+    bandwithArray = vector<int>{1,2,3,5,9,10,15,20};
+    lastId = 0;
+    srand(time(NULL));
+}
+
+lspRequest lspRequestGenarator::generateRequest()
+{
+    lspRequest temp;
+
+    //cout<<"\nsize = "<<reqArray.size()<<endl;
+    int prob = rand() % 100;
+
+    if(prob > 50 || reqArray.empty())
+    {
+        temp.srcNode = rand() % numOfNode;
+        temp.DstNode = rand() % numOfNode;
+
+        while(temp.srcNode == temp.DstNode) temp.DstNode = rand() % numOfNode;
+
+        temp.bandwidthSize = bandwithArray[rand()  % bandwithArray.size()];
+
+        temp.request = true;
+        temp.id = lastId;
+        lastId++;
+        reqArray.push_back(temp);
+
+        return temp;
+    }
+    
+    temp = reqArray[rand() % reqArray.size()];
+    temp.request = false;
+    
+    for(int i = 0; i < reqArray.size(); i++)
+    {
+        if(temp.id == reqArray[i].id)
+        {
+            reqArray.erase(reqArray.begin() + i);
+            return temp; 
+        }
+    }
+ 
+}
